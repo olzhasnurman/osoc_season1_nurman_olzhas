d08c3e1c0480f5ac9403ae2fe81466c6b5cf3042
olzhas_nurman <olzhas.nurman@nu.edu.kz>
2024-02-20 14:41:52 +0600

    Added nonarchitectural registers. One with write enable signal and the other one is without.

---
+++ rtl/register_en.sv
@@ -0,0 +1,32 @@
+/* Copyright (c) 2024 Maveric NU. All rights reserved. */
+
+// -------------------------------------------------------------
+// This is a nonarchitectural register with write enable signal.
+// -------------------------------------------------------------
+
+module register_file
+// Parameters.
+#(
+    parameter DATA_WIDTH = 32
+)
+// Port decleration. 
+(   
+    // Common clock & enable signal.
+    input logic clk,
+    input logic write_en,
+
+    //Input interface. 
+    input logic [ DATA_WIDTH - 1:0 ] i_write_data,
+    
+    // Output interface.
+    output logic [ DATA_WIDTH - 1:0 ] o_read_data
+);
+
+    // Write logic.
+    always_ff @( posedge clk ) begin 
+        if ( write_en ) begin
+            o_read_data <= i_write_data;
+        end
+    end
+    
+endmodule
