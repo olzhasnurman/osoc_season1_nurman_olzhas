cc48c6494c7168e1cbf499bbd1d33f6806693785
olzhas_nurman <olzhas.nurman@nu.edu.kz>
2024-02-25 23:46:55 +0600

    Adjusted memory width & implemented logic for different types of store instructions

--- rtl/memory.sv
+++ rtl/memory.sv
@@ -7,22 +7,25 @@
 module memory
 // Parameters.
 #(
-    parameter DATA_WIDTH  = 32,
+    parameter DATA_WIDTH  = 64,
               ADDR_WIDTH  = 64,
+              INSTR_WIDTH = 32,
               MEM_DEPTH   = 1024
 )
 // Port decleration. 
 (   
     // Common clock & enable signal.
-    input  logic                      clk,
-    input  logic                      write_en,
+    input  logic                       clk,
+    input  logic                       write_en,
+    input  logic [               1:0 ] i_store_type,
                      
     //Input interface. 
-    input  logic [ ADDR_WIDTH - 1:0 ] i_addr,
-    input  logic [ DATA_WIDTH - 1:0 ] i_write_data,
+    input  logic [ ADDR_WIDTH  - 1:0 ] i_addr,
+    input  logic [ DATA_WIDTH  - 1:0 ] i_write_data,
     
     // Output interface.
-    output logic [ DATA_WIDTH - 1:0 ] o_read_data
+    output logic [ DATA_WIDTH  - 1:0 ] o_read_data,
+    output logic [ INSTR_WIDTH - 1:0 ] o_read_instr
 );
 
     // Register block.
@@ -31,12 +34,21 @@ module memory
     // Write logic.
     always_ff @( posedge clk ) begin 
         if ( write_en ) begin
-            mem[ i_addr[ ADDR_WIDTH - 1:2 ] ] <= i_write_data; // Byte addressed memory. Ignore LS two bits for word allignment.
+            case ( i_store_type )
+                2'b11: mem [ i_addr [ ADDR_WIDTH - 1:3 ] ]        <= i_write_data;        // SD Instruction.
+                2'b10: mem [ i_addr [ ADDR_WIDTH - 1:3 ] ] [31:0] <= i_write_data[31:0];  // SW Instruction.
+                2'b01: mem [ i_addr [ ADDR_WIDTH - 1:3 ] ] [15:0] <= i_write_data[15:0];  // SH Instruction.
+                2'b00: mem [ i_addr [ ADDR_WIDTH - 1:3 ] ] [7:0 ] <= i_write_data[7:0];   // SB Instruction.
+
+                default: mem[ i_addr[ ADDR_WIDTH - 1:3 ] ] <= i_write_data;
+            endcase
         end
     end
 
     // Read logic.
-    assign o_read_data = mem [ i_addr[ ADDR_WIDTH - 1:2 ] ];
+    assign o_read_data = mem [ i_addr[ ADDR_WIDTH - 1:3 ] ];
+
+    assign o_read_instr = i_addr[2] ? mem [ i_addr[ ADDR_WIDTH - 1:3 ] ] [63:32] : mem [ i_addr[ ADDR_WIDTH - 1:3 ] ] [31:0];
 
     
 endmodule
--- rtl/top.sv
+++ rtl/top.sv
@@ -7,10 +7,11 @@
 module top
 // Parameters. 
 #(
-    parameter REG_DATA_WIDTH = 64,
-              REG_ADDR_WIDTH = 5,
-              MEM_DATA_WIDTH = 32,
-              MEM_ADDR_WIDTH = 64
+    parameter REG_DATA_WIDTH  = 64,
+              REG_ADDR_WIDTH  = 5,
+              MEM_DATA_WIDTH  = 64,
+              MEM_INSTR_WIDTH = 32,
+              MEM_ADDR_WIDTH  = 64
 
 
 )
@@ -47,9 +48,10 @@ module top
     logic       s_instr_write_en;
 
     // Memory signals.
-    logic [ MEM_ADDR_WIDTH - 1:0 ] s_mem_addr;
-    logic [ MEM_DATA_WIDTH - 1:0 ] s_mem_write_data;
-    logic [ MEM_DATA_WIDTH - 1:0 ] s_mem_read_data;
+    logic [ MEM_ADDR_WIDTH  - 1:0 ] s_mem_addr;
+    logic [ MEM_DATA_WIDTH  - 1:0 ] s_mem_write_data;
+    logic [ MEM_DATA_WIDTH  - 1:0 ] s_mem_read_data;
+    logic [ MEM_INSTR_WIDTH - 1:0 ] s_mem_read_instr;
 
     // Register file signals. 
     logic [ REG_ADDR_WIDTH - 1:0 ] s_reg_addr_1;
@@ -65,17 +67,16 @@ module top
     logic [ REG_DATA_WIDTH - 1:0 ] s_alu_result;
 
     // Registered signals. 
-    logic [ MEM_DATA_WIDTH - 1:0 ] s_reg_instr;
-    logic [ MEM_ADDR_WIDTH - 1:0 ] s_reg_pc;
-    logic [ MEM_ADDR_WIDTH - 1:0 ] s_reg_old_pc;
-    logic [ REG_DATA_WIDTH - 1:0 ] s_reg_data_1;
-    logic [ REG_DATA_WIDTH - 1:0 ] s_reg_data_2;
-    logic [ REG_DATA_WIDTH - 1:0 ] s_reg_alu_result;
-    logic [ MEM_DATA_WIDTH - 1:0 ] s_reg_mem_data;
+    logic [ MEM_INSTR_WIDTH - 1:0 ] s_reg_instr;
+    logic [ MEM_ADDR_WIDTH  - 1:0 ] s_reg_pc;
+    logic [ MEM_ADDR_WIDTH  - 1:0 ] s_reg_old_pc;
+    logic [ REG_DATA_WIDTH  - 1:0 ] s_reg_data_1;
+    logic [ REG_DATA_WIDTH  - 1:0 ] s_reg_data_2;
+    logic [ REG_DATA_WIDTH  - 1:0 ] s_reg_alu_result;
+    logic [ MEM_DATA_WIDTH  - 1:0 ] s_reg_mem_data;
 
     // MUX signals.
     logic [ REG_DATA_WIDTH - 1:0 ] s_result;
-    logic [ REG_DATA_WIDTH - 1:0 ] s_reg_mem_data_sign_ext;
 
     // Immediate extend unit signals. 
     logic [                  24:0 ] s_imm;
@@ -93,10 +94,9 @@ module top
     assign s_reg_addr_1 = s_reg_instr[19:15];
     assign s_reg_addr_2 = s_reg_instr[24:20];
     assign s_reg_addr_3 = s_reg_instr[11:7];
-
-    assign s_reg_mem_data_sign_ext = { {32{s_reg_mem_data[31]}} , s_reg_mem_data };
-    assign s_reg_write_data_3      = s_result;
-    assign s_mem_write_data        = s_reg_data_2[MEM_DATA_WIDTH - 1:0];
+ 
+    assign s_reg_write_data_3 = s_result;
+    assign s_mem_write_data   = s_reg_data_2[MEM_DATA_WIDTH - 1:0];
 
 
 
@@ -149,9 +149,11 @@ module top
     memory MEM (
         .clk          ( clk              ),
         .write_en     ( s_mem_write_en   ),
+        .i_store_type ( s_func_3[1:0]    ),
         .i_addr       ( s_mem_addr       ),
         .i_write_data ( s_mem_write_data ),
-        .o_read_data  ( s_mem_read_data  )
+        .o_read_data  ( s_mem_read_data  ),
+        .o_read_instr ( s_mem_read_instr )
     );
 
 
@@ -177,11 +179,11 @@ module top
     //-----------------------------------------
 
     // Instruction Register Instance. 
-    register_en # (.DATA_WIDTH (MEM_DATA_WIDTH)) INSTR_REG (
+    register_en # (.DATA_WIDTH (MEM_INSTR_WIDTH)) INSTR_REG (
         .clk          ( clk              ),
         .write_en     ( s_instr_write_en ),
         .arstn        ( arstn            ),
-        .i_write_data ( s_mem_read_data  ),
+        .i_write_data ( s_mem_read_instr ),
         .o_read_data  ( s_reg_instr      )
     );
 
@@ -261,11 +263,11 @@ module top
 
     // 3-to-1 Result Source MUX Instance.
     mux3to1 RESULT_MUX (
-        .control_signal ( s_result_src             ),
-        .i_mux_1        ( s_reg_alu_result         ),
-        .i_mux_2        ( s_reg_mem_data_sign_ext  ), 
-        .i_mux_3        ( s_alu_result             ),
-        .o_mux          ( s_result                 )
+        .control_signal ( s_result_src     ),
+        .i_mux_1        ( s_reg_alu_result ),
+        .i_mux_2        ( s_reg_mem_data   ), 
+        .i_mux_3        ( s_alu_result     ),
+        .o_mux          ( s_result         )
     );
 
     // 2-to-1 Memory Address Source MUX Instance.
