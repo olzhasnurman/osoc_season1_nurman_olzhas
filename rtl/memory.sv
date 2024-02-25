f545c48ca68253fbcb05c00e92249034da6278bb
olzhas_nurman <olzhas.nurman@nu.edu.kz>
2024-02-25 16:36:02 +0600

    Adjusted memory organization for byte addressed memory

--- rtl/memory.sv
+++ rtl/memory.sv
@@ -3,12 +3,11 @@
 // -------------------------------------------------------------------------------------------
 // This is a instruction and data memory component of processor based on RISC-V architecture.
 // -------------------------------------------------------------------------------------------
-/* verilator lint_off WIDTH */
+
 module memory
 // Parameters.
 #(
-    parameter BLOCK_WIDTH = 8,
-              DATA_WIDTH  = 32,
+    parameter DATA_WIDTH  = 32,
               ADDR_WIDTH  = 64,
               MEM_DEPTH   = 1024
 )
@@ -27,20 +26,17 @@ module memory
 );
 
     // Register block.
-    logic [ BLOCK_WIDTH - 1:0 ] mem [ MEM_DEPTH - 1:0 ];
+    logic [ DATA - 1:0 ] mem [ MEM_DEPTH - 1:0 ];
 
     // Write logic.
     always_ff @( posedge clk ) begin 
         if ( write_en ) begin
-            mem[i_addr    ] <= i_write_data[7 :0 ];
-            mem[i_addr + 1] <= i_write_data[15:8 ];
-            mem[i_addr + 2] <= i_write_data[23:16];
-            mem[i_addr + 3] <= i_write_data[31:24];
+            mem[ i_addr[ ADDR_WIDTH - 1:2 ] ] <= i_write_data; // Byte addressed memory. Ignore LS two bits for word allignment.
         end
     end
 
     // Read logic.
-    assign o_read_data = {mem[i_addr + 3], mem[i_addr + 2], mem[i_addr + 1], mem[i_addr]};
+    assign o_read_data = mem [ i_addr[ ADDR_WIDTH - 1:2 ] ];
 
     
 endmodule
