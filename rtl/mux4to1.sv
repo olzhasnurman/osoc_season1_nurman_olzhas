55560cbf0d9596fd134500add8c02422f3662d6d
olzhas_nurman <olzhas.nurman@nu.edu.kz>
2024-02-27 16:48:19 +0600

    Added 4-to-1 mux for result source

---
+++ rtl/mux4to1.sv
@@ -0,0 +1,38 @@
+/* Copyright (c) 2024 Maveric NU. All rights reserved. */
+
+// --------------------------------------------------
+// This is a 4-to-1 mux module to choose Result Src.
+// ---------------------------------------------------
+
+module mux3to1
+// Parameters. 
+#(
+    parameter DATA_WIDTH = 64
+) 
+// Port decleration.
+(
+    // Control signal.
+    input  logic [              1:0 ] control_signal,
+
+    // Input interface.
+    input  logic [ DATA_WIDTH - 1:0 ] i_mux_1,
+    input  logic [ DATA_WIDTH - 1:0 ] i_mux_2,
+    input  logic [ DATA_WIDTH - 1:0 ] i_mux_3,
+    input  logic [ DATA_WIDTH - 1:0 ] i_mux_4,
+
+    // Output interface.
+    output logic [ DATA_WIDTH - 1:0 ] o_mux
+);
+
+    // MUX logic.
+    always_comb begin
+        case ( control_signal )
+            2'b00: o_mux = i_mux_1;
+            2'b01: o_mux = i_mux_2;
+            2'b10: o_mux = i_mux_3;
+            2'b11: o_mux = i_mux_4;
+            default: o_mux = i_mux_1;
+        endcase
+    end
+    
+endmodule
