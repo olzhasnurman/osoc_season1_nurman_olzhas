1b3a70db478f2dd407ba5adb8ac5df67a1da3dfb
olzhas_nurman <olzhas.nurman@nu.edu.kz>
2024-04-07 16:02:11 +0500

    Feat: Added register file reset.

--- rtl/register_file.sv
+++ rtl/register_file.sv
@@ -36,6 +36,37 @@ module register_file
     always_ff @( posedge clk ) begin 
         if ( ~arstn ) begin
             mem[0] <= '0;
+            mem[1] <= '0;
+            mem[2] <= '0;
+            mem[3] <= '0;
+            mem[4] <= '0;
+            mem[5] <= '0;
+            mem[6] <= '0;
+            mem[7] <= '0;
+            mem[8] <= '0;
+            mem[9] <= '0;
+            mem[10] <= '0;
+            mem[11] <= '0;
+            mem[12] <= '0;
+            mem[13] <= '0;
+            mem[14] <= '0;
+            mem[15] <= '0;
+            mem[16] <= '0;
+            mem[17] <= '0;
+            mem[18] <= '0;
+            mem[19] <= '0;
+            mem[20] <= '0;
+            mem[21] <= '0;
+            mem[22] <= '0;
+            mem[23] <= '0;
+            mem[24] <= '0;
+            mem[25] <= '0;
+            mem[26] <= '0;
+            mem[27] <= '0;
+            mem[28] <= '0;
+            mem[29] <= '0;
+            mem[30] <= '0;
+            mem[31] <= '0;
         end
         if ( write_en_3 ) begin
             mem[i_addr_3] <= i_write_data_3;
