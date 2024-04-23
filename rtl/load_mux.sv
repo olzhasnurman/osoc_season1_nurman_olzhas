11952304dbc2f8e81d1f1d8c597644206ebd7325
olzhas_nurman <olzhas.nurman@nu.edu.kz>
2024-04-23 16:30:44 +0500

    Feat: Added logic for illegal load instruction & load address misaligned.

--- rtl/load_mux.sv
+++ rtl/load_mux.sv
@@ -19,7 +19,8 @@ module load_mux
 
     // Output interface
     output logic [ DATA_WIDTH - 1:0 ] o_data,
-    output logic                      o_load_addr_ma
+    output logic                      o_load_addr_ma,
+    output logic                      o_illegal_instr
 );
 
     logic [7:0]  s_byte;
@@ -43,42 +44,58 @@ module load_mux
         endcase 
     end
 
-    assign s_half = i_addr_offset[1] ? i_data[31:16] : i_data[15:0]; // PROBLEM: NEED TO IMPLEMENT Load address misaligned.
+    assign s_half = i_addr_offset[1] ? i_data[31:16] : i_data[15:0];
+
 
     always_comb begin
+
         case ( i_func_3 )
-            3'b000: o_data = { { 56{s_byte[7]} }, s_byte};    // LB  Instruction.
+            3'b000: begin
+                o_data          = { { 56{s_byte[7]} }, s_byte};        // LB  Instruction. 
+                o_load_addr_ma  = 1'b0;
+                o_illegal_instr = 1'b0;               
+            end 
 
             3'b001: begin
-               o_data = { { 48{s_half[15]} }, s_half};        // LH  Instruction.
-               o_load_addr_ma = s_load_addr_ma_lh;
+               o_data          = { { 48{s_half[15]} }, s_half};        // LH  Instruction.
+               o_load_addr_ma  = s_load_addr_ma_lh;
+               o_illegal_instr = 1'b0;
             end
 
             3'b010: begin 
-                o_data = { { 32{i_data[31]} }, i_data[31:0]}; // LW  Instruction.
-                o_load_addr_ma = s_load_addr_ma_lw;
+                o_data          = { { 32{i_data[31]} }, i_data[31:0]}; // LW  Instruction.
+                o_load_addr_ma  = s_load_addr_ma_lw;
+                o_illegal_instr = 1'b0;
             end
 
             3'b011: begin 
-                o_data = i_data;                              // LD  Instruction.
-                o_load_addr_ma = s_load_addr_ma_ld;
+                o_data          = i_data;                              // LD  Instruction.
+                o_load_addr_ma  = s_load_addr_ma_ld;
+                o_illegal_instr = 1'b0;
             end
 
-            3'b100: o_data = { { 56{1'b0} }, s_byte};         // LBU Instruction.
-            
+            3'b100: begin
+                o_data          = { { 56{1'b0} }, s_byte};            // LBU Instruction. 
+                o_load_addr_ma  = 1'b0;
+                o_illegal_instr = 1'b0;  
+            end
+
             3'b101: begin 
-                o_data = { { 48{1'b0} }, s_half};             // LHU Instruction.
-                o_load_addr_ma = s_load_addr_ma_lh;
+                o_data          = { { 48{1'b0} }, s_half};             // LHU Instruction.
+                o_load_addr_ma  = s_load_addr_ma_lh;
+                o_illegal_instr = 1'b0;
             end
 
             3'b110: begin 
-                o_data = { { 32{1'b0} }, i_data[31:0]};       // LWU Instruction.
-                o_load_addr_ma = s_load_addr_ma_lw;
+                o_data          = { { 32{1'b0} }, i_data[31:0]};       // LWU Instruction.
+                o_load_addr_ma  = s_load_addr_ma_lw;
+                o_illegal_instr = 1'b0;
             end
         
             default:  begin
-                o_data = '0; // PROBLEM: NEED TO IMPLEMENT ILLEGAL INSTR.
-                o_load_addr_ma = 1'b0;
+                o_data          = '0;
+                o_load_addr_ma  = 1'b0;
+                o_illegal_instr = 1'b1;
             end
 
         endcase
