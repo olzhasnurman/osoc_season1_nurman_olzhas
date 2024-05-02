695d944df03476e9998ae989358eeaf535caea82
olzhas_nurman <olzhas.nurman@nu.edu.kz>
2024-05-02 14:59:03 +0500

    Test: Added verilator testbench and check file for checking status of a0 reg with DPI-C.

---
+++ test/tb/check.c
@@ -0,0 +1,12 @@
+ #include <stdio.h>
+ #include <stdint.h>
+
+
+int check(int64_t a) {
+
+	if ( a == 0 ) printf ("PASS \n");
+    else          printf ("FAIL \n");
+
+    return 0;
+
+}
---
+++ test/tb/tb_test_env.cpp
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <iostream>
+#include <cstdlib>
+#include <memory>
+#include <verilated.h>
+#include <verilated_vcd_c.h>
+#include "Vtest_env.h"
+
+#define MAX_SIM_TIME 1000000
+#define VERIF_START_TIME 7
+vluint64_t sim_time = 0;
+vluint64_t posedge_cnt = 0;
+
+
+void dut_reset (Vtest_env *dut, vluint64_t &sim_time){
+    
+    if( sim_time < 100 ){
+        dut->arstn = 0;
+    }
+    else {
+        dut->arstn = 1;
+    }
+}
+
+int main(int argc, char** argv, char** env) {
+    Verilated::commandArgs(argc, argv);
+    Vtest_env *dut = new Vtest_env;
+
+    while (sim_time < MAX_SIM_TIME) {
+        dut_reset(dut, sim_time);
+        dut->clk ^= 1;
+        dut->eval();
+
+        if (dut->clk == 1){
+            posedge_cnt++;
+        }
+
+        sim_time++;
+    }
+
+    delete dut;
+    exit(EXIT_SUCCESS);
+}
