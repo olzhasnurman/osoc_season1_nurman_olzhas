7e92150ab0eff299e33f015845219383663daeb1
olzhas_nurman <olzhas.nurman@nu.edu.kz>
2024-05-28 18:48:42 +0500

    Feat: Added a module that facilitates the data transfer betwen cache and AXI4-Lite modules.

---
+++ rtl/cache_data_transfer.sv
@@ -0,0 +1,75 @@
+/* Copyright (c) 2024 Maveric NU. All rights reserved. */
+
+// ----------------------------------------------------------------------------
+// This module facilitates the data transfer between cache and AXI interfaces.
+// -----------------------------------------------------------------------------
+
+module cache_data_transfer 
+#(
+    parameter AXI_DATA_WIDTH = 32,
+              AXI_ADDR_WIDTH = 64,
+              BLOCK_WIDTH    = 512
+) 
+(
+    // Control signals.
+    input  logic                          clk,
+    input  logic                          arstn,
+
+    // Input interface.
+    input  logic                          i_start_read,
+    input  logic                          i_start_write,
+    input  logic                          i_axi_done,
+    input  logic [ BLOCK_WIDTH    - 1:0 ] i_data_block_cache,
+    input  logic [ AXI_DATA_WIDTH - 1:0 ] i_data_axi,
+    input  logic [ AXI_ADDR_WIDTH - 1:0 ] i_addr_cache,
+
+    // Output interface.
+    output logic                          o_count_done,
+    output logic [ BLOCK_WIDTH    - 1:0 ] o_data_block_cache,
+    output logic [ AXI_DATA_WIDTH - 1:0 ] o_data_axi,
+    output logic [ AXI_ADDR_WIDTH - 1:0 ] o_addr_axi
+);
+
+    //------------------------
+    // INTERNAL NETS.
+    //------------------------
+    logic s_start;
+
+    assign s_start = i_start_read | i_start_write;
+
+    //-----------------------------------
+    // Lower-level module instantiations.
+    //-----------------------------------
+
+    // Counter module instance.
+    counter COUNT0 (
+        .clk      ( clk          ),
+        .arstn    ( arstn        ),
+        .run      ( i_axi_done   ),
+        .restartn ( s_start      ),
+        .o_done   ( o_count_done )
+    );
+
+    // Address increment module instance.
+    addr_increment ADD_INC0 (
+        .clk    ( clk          ),
+        .run    ( s_start      ),
+        .enable ( i_axi_done   ),
+        .i_addr ( i_addr_cache ),
+        .o_addr ( o_addr_axi   )
+    );
+
+    // FIFO module instance.
+    fifo FIFO0 (
+        .clk          ( clk                ),
+        .arstn        ( arstn              ),
+        .write_en     ( i_axi_done         ),
+        .start_write  ( i_start_write      ),
+        .start_read   ( i_start_read       ),
+        .i_data       ( i_data_axi         ),
+        .i_data_block ( i_data_block_cache ),
+        .o_data       ( o_data_axi         ),
+        .o_data_block ( o_data_block_cache )
+    );
+    
+endmodule
