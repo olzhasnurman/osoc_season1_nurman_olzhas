14fd4c395226a807e63dd1d3b0bbd7bef0b47b8b
olzhas_nurman <olzhas.nurman@nu.edu.kz>
2024-06-07 14:44:19 +0500

    Feat: Modified CSR file to add additional write channel.

--- rtl/csr_file.sv
+++ rtl/csr_file.sv
@@ -15,13 +15,16 @@ module csr_file
 (   
     // Common clock & enable signal.
     input  logic                      clk,
-    input  logic                      write_en,
+    input  logic                      write_en_1,
+    input  logic                      write_en_2,
     input  logic                      arstn,
 
     //Input interface. 
     input  logic [ ADDR_WIDTH - 1:0 ] i_read_addr,
-    input  logic [ ADDR_WIDTH - 1:0 ] i_write_addr,
-    input  logic [ DATA_WIDTH - 1:0 ] i_write_data,
+    input  logic [ ADDR_WIDTH - 1:0 ] i_write_addr_1,
+    input  logic [ ADDR_WIDTH - 1:0 ] i_write_addr_2,
+    input  logic [ DATA_WIDTH - 1:0 ] i_write_data_1,
+    input  logic [ DATA_WIDTH - 1:0 ] i_write_data_2,
     
     // Output interface.
     output logic [ DATA_WIDTH - 1:0 ] o_read_data
@@ -38,12 +41,13 @@ module csr_file
             mem[2] <= '0; // Mtvec.
             mem[3] <= '0; // Reserved.
         end
-        else if ( write_en ) begin
-            mem[i_write_addr] <= i_write_data;
+        else begin
+            if ( write_en_1 ) mem [ i_write_addr_1 ] <= i_write_data_1;
+            if ( write_en_2 ) mem [ i_write_addr_2 ] <= i_write_data_2;  
         end
     end
 
     // Read logic.
-    assign o_read_data = mem[i_read_addr];
+    assign o_read_data = mem [ i_read_addr ];
     
 endmodule
