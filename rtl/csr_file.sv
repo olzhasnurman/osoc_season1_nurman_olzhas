d696bfff569c0efee636afd648c99839b608d7a8
olzhas_nurman <olzhas.nurman@nu.edu.kz>
2024-06-13 17:37:50 +0500

    Feat: Modified csr module to accomodate more csr registers: mstatus, mip, mie.

--- rtl/csr_file.sv
+++ rtl/csr_file.sv
@@ -8,8 +8,8 @@ module csr_file
 // Parameters.
 #(
     parameter DATA_WIDTH = 64,
-              ADDR_WIDTH = 2,
-              REG_DEPTH  = 4
+              ADDR_WIDTH = 3,
+              REG_DEPTH  = 8
 )
 // Port decleration. 
 (   
@@ -25,9 +25,14 @@ module csr_file
     input  logic [ ADDR_WIDTH - 1:0 ] i_write_addr_2,
     input  logic [ DATA_WIDTH - 1:0 ] i_write_data_1,
     input  logic [ DATA_WIDTH - 1:0 ] i_write_data_2,
+    input  logic                      i_timer_int_call,
+    input  logic                      i_timer_int_jump,
     
     // Output interface.
-    output logic [ DATA_WIDTH - 1:0 ] o_read_data
+    output logic [ DATA_WIDTH - 1:0 ] o_read_data,
+    output logic                      o_mie_mstatus,
+    output logic                      o_mtip_mip,
+    output logic                      o_mtie_mie
 );
 
     // Register block.
@@ -36,18 +41,28 @@ module csr_file
     // Write logic.
     always_ff @( posedge clk, negedge arstn ) begin 
         if ( ~arstn ) begin
-            mem[0] <= '0; // Mepc.
-            mem[1] <= '0; // Mcause.
-            mem[2] <= '0; // Mtvec.
-            mem[3] <= '0; // Reserved.
+            mem[ 0 ] <= '0; // Mstatus
+            mem[ 1 ] <= '0; // Reserved.
+            mem[ 2 ] <= '0; // Mie.
+            mem[ 3 ] <= '0; // Mtvec.
+            mem[ 4 ] <= '0; // Mcause.
+            mem[ 5 ] <= '0; // Mepc.
+            mem[ 6 ] <= '0; // Mip.
+            mem[ 7 ] <= '0; // Reserved.
         end
         else begin
+            if ( i_timer_int_call ) mem[ 6 ][ 7 ] <= 1'b1; // mip MTIP bit set.
+            else                    mem[ 6 ][ 7 ] <= 1'b0; // mip MTIP bit clear. 
+            if ( i_timer_int_jump ) mem[ 0 ][ 3 ] <= 1'b0; // mstatus MIE bit clear.
             if ( write_en_1 ) mem [ i_write_addr_1 ] <= i_write_data_1;
             if ( write_en_2 ) mem [ i_write_addr_2 ] <= i_write_data_2;  
         end
     end
 
     // Read logic.
-    assign o_read_data = mem [ i_read_addr ];
+    assign o_read_data   = mem [ i_read_addr ];
+    assign o_mie_mstatus = mem [ 0 ][ 3 ];
+    assign o_mtip_mip    = mem [ 6 ][ 7 ];
+    assign o_mtie_mie    = mem [ 2 ][ 7 ];  
     
 endmodule
