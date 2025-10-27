4d09e6fbd8efedffa0293a9ed2ecaeec416fd99c
olzhas_nurman <olzhas.nurman@nu.edu.kz>
2025-10-27 13:08:16 +0500

    test: add waveform.

--- test/tb/tb_test_env.cpp
+++ test/tb/tb_test_env.cpp
@@ -14,29 +14,36 @@ vluint64_t posedge_cnt = 0;
 void dut_reset (Vtest_env *dut, vluint64_t &sim_time){
     
     if( sim_time < 100 ){
-        dut->arst = 1;
+        dut->reset = 1;
     }
     else {
-        dut->arst = 0;
+        dut->reset = 0;
     }
 }
 
 int main(int argc, char** argv, char** env) {
     Verilated::commandArgs(argc, argv);
     Vtest_env *dut = new Vtest_env;
+		Verilated::traceEverOn(true);
+		VerilatedVcdC* sim_trace = new VerilatedVcdC;
+		dut->trace(sim_trace, 10);
+		sim_trace->open("./waveform1.vcd");
 
     while (sim_time < MAX_SIM_TIME) {
         dut_reset(dut, sim_time);
-        dut->clk ^= 1;
+        dut->clock ^= 1;
         dut->eval();
 
-        if (dut->clk == 1){
+        if (dut->clock == 1){
             posedge_cnt++;
         }
 
+				sim_trace->dump(sim_time);
         sim_time++;
     }
 
+		sim_trace->close();
+		delete sim_trace;
     delete dut;
     exit(EXIT_SUCCESS);
 }
